1fe25bcd80e2383b3640528e599e30a27768f6a5
cohen3 <e582006E>
2020-01-14 10:12:56 +0200

    tests

---
+++ src/printSortedButtomUp.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+public class printSortedButtomUp {
+    /**
+     *            +-------------+
+     *            | printSorted |
+     *            +-------------+
+     *              /         \
+     *       +----------+  +-----------+
+     *       | printArr |  | sortArray |
+     *      +-----------+  +-----------+
+     *                              \
+     *                          +---------+
+     *                          | copyArr |
+     *                          +---------+
+     *     top down:
+     *     1) copyArr, printArr
+     *     2) sortArray
+     *     3) printSorted
+     */
+}
---
+++ src/printSortedTopDown.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+
+public class printSortedTopDown {
+    /**
+     *            +-------------+
+     *            | printSorted |
+     *            +-------------+
+     *              /         \
+     *       +----------+  +-----------+
+     *       | printArr |  | sortArray |
+     *      +-----------+  +-----------+
+     *                              \
+     *                          +---------+
+     *                          | copyArr |
+     *                          +---------+
+     *     top down:
+     *     1) printSorted
+     *     2) printArr, sortArray
+     *     3) copyArr
+     */
+
+}
