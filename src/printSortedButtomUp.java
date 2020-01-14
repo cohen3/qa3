bdfc12c2328f4043d0283e229819f35ac2d5c5fc
cohen3 <e582006E>
2020-01-14 10:20:04 +0200

    tests

--- src/printSortedButtomUp.java
+++ src/printSortedButtomUp.java
@@ -1,4 +1,9 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class printSortedButtomUp {
     /**
@@ -18,4 +23,22 @@ public class printSortedButtomUp {
      *     2) sortArray
      *     3) printSorted
      */
+
+    // for print tests
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setup(){
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
 }
--- src/printSortedTopDown.java
+++ src/printSortedTopDown.java
@@ -1,5 +1,9 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class printSortedTopDown {
     /**
@@ -20,4 +24,21 @@ public class printSortedTopDown {
      *     3) copyArr
      */
 
+    // for print tests
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setup(){
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
 }
